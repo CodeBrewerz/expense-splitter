dddbc55be1cb64fd3c30d19b8008ce799b47c5e2
Ananya Sharma <ananyas237@gmail.com>
2022-02-23 21:41:29 -0500

    setup nhost infrastructure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { NhostClient } from '@nhost/nhost-js'
+import { NhostAuthProvider, useNhostAuth } from '@nhost/react-auth'
 import Index from './routes';
 import { horizontalListSortingStrategy, rectSortingStrategy } from '@dnd-kit/sortable';
 import Dashboard from './components/Dashboard';
@@ -7,8 +9,13 @@ import Sortable from './components/dnd-kit/Sortable/Sortable';
 import List from './components/dnd-kit/List/List';
 import MultipleContainers from './components/dnd-kit/multiple-dnd/MultipleContainers';
 import PlaidLink from "./components/PlaidLink";
+import SignIn from "./routes/sign-in";
 
 function App() {
+  const nhost = new NhostClient({
+    backendUrl: 'https://fytcssmgfmprizwxxnyb.nhost.run'
+  });
+  
   const horizontalProps = {
     Container: (props) => <List horizontal {...props} />,
     style: {
@@ -24,25 +31,28 @@ function App() {
   };
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Index />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="transactions" element={<TransactionTable />} />
-          <Route path="plaid-accounts" element={<PlaidLink />} />
-          <Route path="reconcilations" element={
-            <main className='flex flex-1 flex-wrap'>
-              <Sortable {...horizontalProps} />
-              <MultipleContainers trashable
-                columns={2}
-                strategy={rectSortingStrategy}
-                wrapperStyle={() => ({
-                  width: 150,
-                  height: 150,
-                })} />
-            </main>
-          } />
-        </Route>
-      </Routes>
+      <NhostAuthProvider nhost={nhost}>
+        <Routes>
+          <Route path="/" element={<Index />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="transactions" element={<TransactionTable />} />
+            <Route path="plaid-accounts" element={<PlaidLink />} />
+            <Route path="reconcilations" element={
+              <main className='flex flex-1 flex-wrap'>
+                <Sortable {...horizontalProps} />
+                <MultipleContainers trashable
+                  columns={2}
+                  strategy={rectSortingStrategy}
+                  wrapperStyle={() => ({
+                    width: 150,
+                    height: 150,
+                  })} />
+              </main>
+            } />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />}/>
+        </Routes>
+      </NhostAuthProvider>
 
       {/* <SlideOver open={open} setOpen={setOpen} /> */}
 
---
+++ src/contexts/nhost-client.context.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const NHostClientContext = createContext(null);
+
+export default NHostClientContext;
